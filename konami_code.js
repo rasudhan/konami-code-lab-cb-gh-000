3d19608488d28bad5f885cf8ffb99470af6a000c
Anandha Sudhan <rasudhan@gmail.com>
2018-10-06 22:50:38 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,10 +15,24 @@ function init() {
   // your code here
   const main = document.body;
 
-  main.addEventListener('keydown', function(e) {
+  main.addEventListener('keydown', onKeyDownHandler);
 
-    let index = 0;
+}
 
-  });
 
+// This is the function that would be invoked by the event listener.
+function onKeyDownHandler(e) {
+  const key = e.key;
+
+  if (key === alphabet[index]) {
+    index++;
+
+    if (index === alphabet.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
